04d6120c2b245f7ce43965a93af47c806b01b690
tromm <chldpwl5620@gmail.com>
2026-03-12 17:13:46 +0900

    Fix star colors to purple and restore blue gradient

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -50,12 +50,20 @@
   });
 </script>
 
+<div class="bg-gradient"></div>
 <div class="stars">
   <span class="star-1">✦</span>
   <span class="star-2">✦</span>
   <span class="star-3">✦</span>
   <span class="star-4">✦</span>
   <span class="star-5">✦</span>
+  <span class="star-6">✦</span>
+  <span class="star-7">✦</span>
+  <span class="star-8">✦</span>
+  <span class="star-9">✦</span>
+  <span class="star-10">✦</span>
+  <span class="star-11">✦</span>
+  <span class="star-12">✦</span>
 </div>
 
 <nav>
