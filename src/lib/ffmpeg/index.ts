a3a30ce82ead2208c8d372d2a33970528a77d10d
tromm <chldpwl5620@gmail.com>
2026-03-17 14:20:41 +0900

    refactor: static/ffmpeg를 gitignore 처리하고 postinstall로 복사

--- src/lib/ffmpeg/index.ts
+++ src/lib/ffmpeg/index.ts
@@ -5,7 +5,8 @@ export async function loadFfmpeg(): Promise<FFmpeg> {
   const ffmpeg = new FFmpeg();
   await ffmpeg.load({
     coreURL: await toBlobURL('/ffmpeg/ffmpeg-core.js', 'text/javascript'),
-    wasmURL: await toBlobURL('/ffmpeg/ffmpeg-core.wasm', 'application/wasm')
+    wasmURL: await toBlobURL('/ffmpeg/ffmpeg-core.wasm', 'application/wasm'),
+    workerURL: await toBlobURL('/ffmpeg/ffmpeg-worker.js', 'text/javascript')
   });
   return ffmpeg;
 }
