e413170fa472c84a34d5c2e5980b7060c441c7b3
tromm <chldpwl5620@gmail.com>
2026-04-10 15:35:37 +0900

    feat: sitemap.xml 엔드포인트 추가

---
+++ src/routes/sitemap.xml/+server.ts
@@ -0,0 +1,31 @@
+import type { RequestHandler } from '@sveltejs/kit';
+
+const SITE_URL = 'https://clipdown.app';
+
+const pages = [
+	{ path: '/', changefreq: 'weekly', priority: '1.0' }
+];
+
+export const GET: RequestHandler = () => {
+	const urls = pages
+		.map(
+			({ path, changefreq, priority }) => `
+  <url>
+    <loc>${SITE_URL}${path}</loc>
+    <changefreq>${changefreq}</changefreq>
+    <priority>${priority}</priority>
+  </url>`
+		)
+		.join('');
+
+	const xml = `<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">${urls}
+</urlset>`;
+
+	return new Response(xml, {
+		headers: {
+			'Content-Type': 'application/xml',
+			'Cache-Control': 'max-age=3600'
+		}
+	});
+};
